ff2de734eef733f1e8e3517bf3c5021b87dfaff4
PaprikaMika <55081913+PaprikaMika@users.noreply.github.com>
2021-01-27 23:21:09 +0100

    triangle

--- script.js
+++ script.js
@@ -3,18 +3,60 @@ $(window).scroll(function(){;
    $('.chapter').css( 'background-color', '' );
     if(w<=$('#pilot').offset().top){
         document.documentElement.style.setProperty('--bars', '255, 255, 0');
+        $('#yellow').css( 'content', 'url("files/triangle.svg")' );
+        $('#light-blue').css( 'content', 'url()' );
+        $('#green').css( 'content', 'url()' );
+        $('#pink').css( 'content', 'url()' );
+        $('#red').css( 'content', 'url()' );
+        $('#dark-blue').css( 'content', 'url()' );
     }else if(w<=$('#episode-1').offset().top){
         document.documentElement.style.setProperty('--bars', '0, 255, 255');
+          $('#yellow').css( 'content', 'url()' );
+          $('#light-blue').css( 'content', 'url("files/triangle.svg")' );
+          $('#green').css( 'content', 'url()' );
+          $('#pink').css( 'content', 'url()' );
+          $('#red').css( 'content', 'url()' );
+          $('#dark-blue').css( 'content', 'url()' );
     }else if(w<=$('#episode-2').offset().top){
          document.documentElement.style.setProperty('--bars', '0, 255, 0');
+         $('#yellow').css( 'content', 'url()' );
+         $('#light-blue').css( 'content', 'url()' );
+         $('#green').css( 'content', 'url("files/triangle.svg")' );
+         $('#pink').css( 'content', 'url()' );
+         $('#red').css( 'content', 'url()' );
+         $('#dark-blue').css( 'content', 'url()' );
     }else if(w<=$('#episode-3').offset().top){
          document.documentElement.style.setProperty('--bars', '255, 0, 255');
+         $('#yellow').css( 'content', 'url()' );
+         $('#light-blue').css( 'content', 'url()' );
+         $('#green').css( 'content', 'url()' );
+         $('#pink').css( 'content', 'url("files/triangle.svg")' );
+         $('#red').css( 'content', 'url()' );
+         $('#dark-blue').css( 'content', 'url()' );
     }else if(w<=$('#finale').offset().top){
          document.documentElement.style.setProperty('--bars', '255, 0, 0');
+         $('#yellow').css( 'content', 'url()' );
+         $('#light-blue').css( 'content', 'url()' );
+         $('#green').css( 'content', 'url()' );
+         $('#pink').css( 'content', 'url()' );
+         $('#red').css( 'content', 'url("files/triangle.svg")' );
+         $('#dark-blue').css( 'content', 'url()' );
     }else if(w<=$('#credits').offset().top){
         document.documentElement.style.setProperty('--bars', '0, 0, 255');
+        $('#yellow').css( 'content', 'url()' );
+        $('#light-blue').css( 'content', 'url()' );
+        $('#green').css( 'content', 'url()' );
+        $('#pink').css( 'content', 'url()' );
+        $('#red').css( 'content', 'url()' );
+        $('#dark-blue').css( 'content', 'url("files/triangle.svg")' );
     }else{
          document.documentElement.style.setProperty('--bars', '255, 255, 255');
+         $('#yellow').css( 'content', 'url()' );
+         $('#light-blue').css( 'content', 'url()' );
+         $('#green').css( 'content', 'url()' );
+         $('#pink').css( 'content', 'url()' );
+         $('#red').css( 'content', 'url()' );
+         $('#dark-blue').css( 'content', 'url()' );
     }
 
 });
